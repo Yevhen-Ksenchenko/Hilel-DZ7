f6f20d8bf6e4ad7749a2c7762f6df7d86b80550e
Yevhen Ksenchenko <harlampiy2@gmail.com>
2025-06-22 12:02:37 +0300

    added DZ7.2.py

--- DZ7.1.py
+++
@@ -1,7 +0,0 @@
-def say_hi(name, age):
-    return f"Hi. My name is {name} and I'm {age} years old"
-
-assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old"
-print("ok")
-assert say_hi("Yevhen", 43) == "Hi. My name is Yevhen and I'm 43 years old"
-print("OK")
